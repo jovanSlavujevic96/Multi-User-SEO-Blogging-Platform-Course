e2b2234f427e365a350d52b21795f9cc19a32ffb
Jovan Slavujevic <jotzilla96@gmail.com>
2022-04-27 21:50:10 +0200

    SEO head section for blogs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,6 +12,10 @@ module.exports = {
     publicRuntimeConfig: {
         APP_NAME: 'SEOBLOG',
         API_DEVELOPMENT: 'http://localhost:8000/api',
-        PRODUCTION: false
+        API_PRODUCTION: 'https://seoblog.com/api',
+        PRODUCTION: false,
+        DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+        DOMAIN_PRODUCTION: 'https://seoblog.com',
+        FB_APP_ID: '517904876628131' // SEOBLOG_LOCAL FB APP
     }
 };
