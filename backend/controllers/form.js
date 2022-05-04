838513aa874df89a7b04c414d17cf2caa37396de
Jovan Slavujevic <jotzilla96@gmail.com>
2022-05-05 00:11:23 +0200

    FIX: replaced sender & receiver + added service mail(s)

--- backend/controllers/form.js
+++ backend/controllers/form.js
@@ -1,28 +1,31 @@
 import sgMail from '@sendgrid/mail'; // SENDGRID_API_KEY
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+// contactForm
 export const contactForm = (req, res) => {
     const {email, name, message} = req.body;
     // console.log(req.body) // test
 
     const emailData = {
-        to: `${email}`,
+        to: `${process.env.EMAIL_TO}`,
         from: `${process.env.EMAIL_FROM}`,
         subject: `Contact form - ${process.env.APP_NAME}`,
         text: `
-            Email received from contact form \n
-            Receiver name: ${name} \n
-            Receiver email: ${email} \n
-            Sender name: ${process.env.APP_NAME} \n
-            Sender email: ${process.env.EMAIL_FROM} \n
+            Receiver name: ${process.env.APP_NAME} \n
+            Receiver email: ${process.env.EMAIL_TO} \n
+            ----------------------------------------\n
+            Email received from contact form: \n
+            Sender name: ${name} \n
+            Sender email: ${email} \n
             Sender message: ${message}
         `,
         html: `
+            <p>Receiver name: ${process.env.APP_NAME}</p>
+            <p>Receiver email: ${process.env.EMAIL_TO}</p>
+            <hr/>
             <h4>Email received from contact form: </h4>
-            <p>Receiver name: ${name}</p>
-            <p>Receiver email: ${email}</p>
-            <p>Sender name: ${process.env.APP_NAME}</p>
-            <p>Sender email: ${process.env.EMAIL_FROM}</p>
+            <p>Sender name: ${name}</p>
+            <p>Sender email: ${email}</p>
             <p>Sender message: ${message}</p>
             <hr/>
             <p>This email may contain sensetive information</p>
